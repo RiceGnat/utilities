8be7afb1c5d058d3a688ddf2034466fa694588e4
RiceGnat <ricegnat@gmail.com>
2016-01-28 15:56:47 -0600

    Initial commit of sleep/shutdown wrapper

--- uri-wrapper/pssd-wrapper/pssd-wrapper.cpp
+++ uri-wrapper/pssd-wrapper/pssd-wrapper.cpp
@@ -1,4 +1,6 @@
-#define URI_SCHEME "wol:"
+#define URI_SLEEP "sleep:"
+#define URI_SHUTDOWN "shutdown:"
+#define URI_RESTART "restart:"
 #define ARG_DELIM '-'
 
 #include <string>
@@ -9,11 +11,11 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	if (argc < 2) {
-		printf("No arguments received\n");
+		printf("No arguments received.\n");
 		return 0;
 	}
 	if (argc < 3) {
-		printf("No URI received\n");
+		printf("No URI received.\n");
 		return 0;
 	}
 
@@ -22,34 +24,27 @@ int main(int argc, char** argv) {
 	const string scheme(uri.substr(0, uri.find(':') + 1));
 	const string body(uri.substr(scheme.length()));
 
-	if (scheme.compare(URI_SCHEME) != 0) {
-		printf("URI must begin with wol:");
-		return 0;
-	}
-
 	stringstream ss(body);
 	vector<string> tokens;
 	string token;
+
 	while (getline(ss, token, ARG_DELIM)) {
 		tokens.push_back(token);
 	}
-
-	if (tokens.size() < 3) {
-		printf("Insufficent arguments to WolCmd\n");
-		return 0;
-	}
-
-	string macaddress(tokens.at(0));
-	string broadcast(tokens.at(1));
-	string subnetmask(tokens.at(2));
+	
+	string computer(tokens.at(0));
 
 	stringstream out;
 
 	out << binpath << " ";
-	out << macaddress << " ";
-	out << broadcast << " ";
-	out << subnetmask;
+	out << computer << " ";
+	//"Invoke-WmiMethod -Class Win32_Process -Name Create -ArgumentList ""rundll32.exe" "user32.dll,LockWorkStation"""
+	if (scheme.compare(URI_SLEEP)) {
+	}
+	else {
+		printf("Unrecognized URI scheme\n");
+		return 0;
+	}
 
-	system(out.str().c_str());
 	return 0;
 }

cd83e7a904821ef919e62864bc62ddb59e27d65f
RiceGnat <ricegnat@gmail.com>
2017-11-03 16:23:22 -0500

    change boss id check to dimension halo only

--- gbf/inject.js
+++ gbf/inject.js
@@ -1,6 +1,7 @@
 require(["lib/common"], function () {
     require(["jquery"], function ($) {
         $(document).ajaxComplete(function (event, xhr, settings) {
+            // Add summon list to support summon tiles
             if (window.location.href.includes("supporter") && settings.url.includes("supporter")) {
                 $(".lis-supporter").each(function () {
                     var i = -1;
@@ -20,13 +21,21 @@ require(["lib/common"], function () {
                         .append($(".prt-supporter-attribute:first " + selector).not(this).map(map));
                 });
             }
-            if (window.location.href.includes("#raid") && settings.url.includes("/ob/")) {
-                $("<div>")
-                    .css({ "position": "absolute", "left": "10px", "bottom": "110px", "z-index": "12", "font-size": "8px", "color": "grey", "opacity": "0.4" })
-                    .text(stage.gGameStatus.boss.param[0].enemy_id)
-                    .appendTo(".cnt-raid");
+            // Show Dimension Halo check
+            if (window.location.href.includes("#raid") && settings.url.includes("start")) {
+                var check = function () {
+                    if (typeof stage !== "undefined") {
+                        if (stage.gGameStatus.boss.param[0].name.en.includes("Dimension Halo"))
+                            $("<div>")
+                                .css({ "position": "absolute", "left": "10px", "bottom": "110px", "z-index": "12", "font-size": "8px", "color": "grey", "opacity": "0.4" })
+                                .text(stage.gGameStatus.boss.param[0].enemy_id)
+                                .appendTo(".cnt-raid");
+                    }
+                    else setTimeout(check, 500);
+                }
+                check();
             }
-
+            // Show all summons on profile
             if (window.location.href.includes("#profile") && settings.url.includes("content")) {
                 var map = function () {
                     var color;
